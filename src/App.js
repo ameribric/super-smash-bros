645d7cb354fc14467b0f09c64c98074a3f08128f
Amer Ibric <ameribric@gmail.com>
2022-04-05 13:15:49 -0400

    Fighter screen createdd

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ function App() {
     <Fighter name={'Roy'} color={'red'} />
     <Fighter name={'Fox'} color={'green'} />
     <Fighter name={'Ganondorf'} color={'purple'} />
+    <Fighter name={'Sheik'} color={'lightskyblue'} />
+      </div>
+      <div>
+        <div className="fighter-screen">
+        <h2>Roy</h2>
+        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png"/>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam temporibus rerum iusto doloremque vitae iste natus quod! Est, dolorem quos architecto nisi vero odit consequatur suscipit, voluptatem numquam, incidunt molestias.
+        </p>
+        </div>
       </div>
     </div>
   );
